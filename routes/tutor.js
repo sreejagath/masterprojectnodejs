80188f3cb9ddaa23aa3698da263a94b2276ee104
Jagal Sree <jagalsree007@gmail.com>
2020-11-22 17:53:44 +0530

    login

--- routes/tutor.js
+++ routes/tutor.js
@@ -4,6 +4,9 @@ var router = express.Router();
 /* GET users listing. */
 router.get('/', function(req, res, next) {
   res.render('tutor/tutor-main');
-});
-
+})
+router.get('/tutor-login', function(req, res, next) {
+    console.log("login page");
+    res.render('tutor/tutor-login');
+  });
 module.exports = router;
