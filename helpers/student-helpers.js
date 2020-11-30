1c2a7098a3af348052048f0b904d59f70e7b9c2d
Jagal Sree <jagalsree007@gmail.com>
2020-11-30 21:00:25 +0530

    student login

--- helpers/student-helpers.js
+++ helpers/student-helpers.js
@@ -9,5 +9,29 @@ module.exports={
             resolve(data.ops[0])
         })
         })
+    },
+    doLogin:(studentData)=>{
+        return new Promise(async(resolve,reject)=>{
+            let loginStatus=false
+            let response={}
+            let student=await db.get().collection(collection.STUDENT_COLLECTION).findOne({username:studentData.username})
+            if(student){
+                bcrypt.compare(studentData.password,student.password).then((status)=>{
+                    if(status){
+                        console.log("Login Success")
+                        response.student=student
+                        response.status=true
+                        resolve(response)
+                    }else{
+                        console.log("Login Failed")
+                        resolve({status:false})
+                    }
+                })
+            }else{
+                console.log("Login Failed")
+                resolve({status:false})
+            }
+    
+        })
     }
 }
